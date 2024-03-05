d5c59cf35a03fd254f1873eeea5199e9c4db1c76
dinsinmanabo <deensummer99@gmail.com>
2024-03-05 15:59:43 +0800

    1st Commit of Register

---
+++ register.js
@@ -0,0 +1,26 @@
+console.log("Hello World!");
+console.error("Error Happened!! ");
+console.warn("WARNENGGG!!");
+
+//VARIABLES
+// STRING = "WORDS"
+// NUMBER = 123, 434e, 2398.12
+// BOOLEAN = true, false
+// OBJECT = ["apple", "banana"], [1,2,3,4], ["apple ", 3, true]
+
+
+//typeoff
+//console.log(typeof(["apple", "banana"]))
+
+
+//DECLARATION OF VARIABLES
+
+let name =`Dinsin`;
+lastname = 'Manabo'
+age = 18;
+isProf = true;
+hobbies = ["Sleeping"];
+console.log(name, age, isProf, hobbies);
+console.table(hobbies);
+console.log("My name is" + name + " " + lastname);
+console.log(`My name is ${name} ${lastname}`);
